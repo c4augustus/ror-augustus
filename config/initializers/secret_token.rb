be2d857adb901a4446a6207bb5ef14c2a912a0f4
cathames <repos@cathames.com>
2013-09-04 15:59:45 -0700

    instantiated by: "rails new ror-cathames"

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RorCathames::Application.config.secret_key_base = 'e21ad5f090106efa8d3a7bdc12b0f7c06829e0d56c90717066e3e312f5ce6f9a0117100e08c8494e6c77ce8ae7adda7473839b9127cf90f6c8c5e50e01923a54'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RorCathames::Application.config.session_store :cookie_store, key: '_ror-cathames_session'
